f5a1dbe3a86a3769bc768e063bda13b892121479
Lucas Sebastian <lucassebastian167@gmail.com>
2022-07-14 11:17:26 -0300

    Created ToDoController

---
+++ ToDoApi/Controllers/ToDoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoApi.Controllers
+{
+    [Route("api/v1/todo")]
+    [ApiController]
+    public class ToDoController : ControllerBase
+    {
+
+    }
+}
