feea166025b46ae44190c07f07b551d4e2cb09eb
Lucas Sebastian <lucassebastian167@gmail.com>
2022-07-18 15:43:21 -0300

    Creating the TodoItemRequest entity and its validator

---
+++ ToDoApi/Contracts/Validator/TodoItemRequestValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using ToDoApi.Contracts.Request;
+
+namespace ToDoApi.Contracts.Validators
+{
+    public class TodoItemRequestValidator : AbstractValidator<TodoItemRequest>
+    {
+        public TodoItemRequestValidator()
+        {
+            RuleFor(x => x.Title)
+                .NotEmpty()
+                .WithMessage("The title is required");
+
+            RuleFor(x => x.Description)
+                .NotEmpty()
+                .WithMessage("The description is required");
+
+            RuleFor(x => x.EndDate.Day)
+                .GreaterThan(DateTime.Today.Day)
+                .WithMessage("The date has to be greater than the current date");
+        }
+    }
+}
