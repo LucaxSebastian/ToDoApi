fd202fa817e27799323af99d87814ff2606a0fde
Lucas Sebastian <lucassebastian167@gmail.com>
2022-07-25 11:45:56 -0300

    Creating the TodoList class
    List created to save the data in memory

---
+++ ToDoApi/Contracts/Response/TodoList.cs
@@ -0,0 +1,9 @@
+﻿using ToDoApi.Domain.Entities;
+
+namespace ToDoApi.Contracts.Response
+{
+    public static class TodoList
+    {
+        public static List<TodoItem> todoItems = new List<TodoItem>();
+    }
+}
