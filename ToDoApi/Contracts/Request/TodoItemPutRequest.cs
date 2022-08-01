6d4c7b8538d063087f81278b289c58dc93d613f6
Lucas Sebastian <lucassebastian167@gmail.com>
2022-08-01 09:52:54 -0300

    Creating the entity to update ToDo and its validator

---
+++ ToDoApi/Contracts/Request/TodoItemPutRequest.cs
@@ -0,0 +1,23 @@
+﻿using ToDoApi.Contracts.Validator;
+
+namespace ToDoApi.Contracts.Request
+{
+    public class TodoItemPutRequest : TodoItemRequest
+    {
+        public DateTime UpdateDate { get; set; }
+
+        public TodoItemPutRequest(string title, string description, DateTime endDate, DateTime updateDate) : base(title, description, endDate)
+        {
+            UpdateDate = updateDate;
+        }
+        public void Validate()
+        {
+            var validationResult = new TodoItemPutRequestValidator().Validate(this);
+
+            if (!validationResult.IsValid)
+            {
+                throw new BadHttpRequestException($"Invalid request: {validationResult}. Please, try again.");
+            }
+        }
+    }
+}
--- ToDoApi/Domain/Entities/TodoItem.cs
+++ ToDoApi/Domain/Entities/TodoItem.cs
@@ -5,13 +5,14 @@ namespace ToDoApi.Domain.Entities
 {
     public class TodoItem
     {
-        public int Id { get; set; }
+        public int Id { get; private set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public DateTime EndDate { get; set; }
         public string State { get; set; }
         public DateTime CreationDate { get; set; }
         public bool IsDone { get; set; }
+        public DateTime? UpdateDate { get; set; }
 
         public TodoItem(string title, string description, DateTime endDate)
         {
